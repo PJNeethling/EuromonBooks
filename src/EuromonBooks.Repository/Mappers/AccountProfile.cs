f4d01c47deda2f17e8d9e14aadf52d3935a53c45
PJ Neethling <neethlingpeterjohn@gmail.com>
2022-11-22 21:10:59 +0200

    Add automapper

---
+++ src/EuromonBooks.Repository/Mappers/AccountProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using EuromonBooks.Abstractions.Models;
+using EuromonBooks.Database.Abstractions.Models;
+using EuromonBooks.Database.Abstractions.ProcedureParamaters;
+using EuromonBooks.Database.Abstractions.Queries;
+using EuromonBooks.Domain.Abstractions.Models.Account;
+
+namespace EuromonBooks.Repository.Mappers
+{
+    public sealed class AccountProfile : Profile
+    {
+        public AccountProfile()
+        {
+            CreateMap<UserModel, UserParams>()
+              .ForMember(x => x.Password, opt => opt.Ignore());
+
+            CreateMap<RoleQuery, Role>();
+
+            CreateMap<UsersQuery, UserWithDates>();
+        }
+    }
+}
